a9cb38287c7c60cd33b05aa97f36055b7ad16cb5
Geir Gåsodden <geir.gasodden@pythonia.no>
2016-11-19 12:22:40 +0100

    Updates tests

--- test/lib/get-ansatt-test.js
+++ test/lib/get-ansatt-test.js
@@ -1,3 +1,15 @@
-/**
- * Created by geir on 19/11/2016.
- */
+'use strict'
+
+const tap = require('tap')
+const getAnsatt = require('../../lib/get-ansatt')
+
+tap.test('it returns results', (test) => {
+  getAnsatt('geir', (error, data) => {
+    if (error) {
+      throw error
+    } else {
+      tap.ok(data, 'data OK')
+      test.done()
+    }
+  })
+})
