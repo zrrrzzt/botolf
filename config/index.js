2703e7e4cb935705db9ff6f3f8f9ecc1c15ddde7
Geir Gåsodden <geir.gasodden@pythonia.no>
2017-01-08 11:49:04 +0100

    Switches to got. Use STATUS_QUEUE_URL (patch)

--- config/index.js
+++ config/index.js
@@ -6,6 +6,7 @@ module.exports = {
   STATS_SKOLESKYSS_URL: process.env.STATS_SKOLESKYSS_URL || 'https://tfk-livestats.firebaseio.com/skoleskyss.json',
   STATS_MINELEV_URL: process.env.STATS_MINELEV_URL || 'https://tfk-livestats.firebaseio.com/minelev.json',
   STATS_TILSKUDD_URL: process.env.STATS_TILSKUDD_URL || 'https://tfk-stats.firebaseio.com/tilskudd.json',
+  STATS_QUEUE_URL: process.env.STATS_QUEUE_URL || 'https://queue.status.tfk.allthethings.win?format=json',
   POLITIKER_API: process.env.POLITIKER_API || 'https://politiker-api.t-fk.no/politicians/search/',
   ANSATTE_API: process.env.ANSATTE_API || 'https://search.portalen.t-fk.no/api/employees/search/'
 }
