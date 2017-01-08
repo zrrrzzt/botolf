d1e39ed2afafefcf189f9cafda861ed497717deb
Geir Gåsodden <geir.gasodden@pythonia.no>
2017-01-08 20:06:31 +0100

    Renames STAT_QUEUE_URL to match plugin name (patch)

--- config/index.js
+++ config/index.js
@@ -6,8 +6,8 @@ module.exports = {
   STATS_SKOLESKYSS_URL: process.env.STATS_SKOLESKYSS_URL || 'https://tfk-livestats.firebaseio.com/skoleskyss.json',
   STATS_MINELEV_URL: process.env.STATS_MINELEV_URL || 'https://tfk-livestats.firebaseio.com/minelev.json',
   STATS_TILSKUDD_URL: process.env.STATS_TILSKUDD_URL || 'https://tfk-stats.firebaseio.com/tilskudd.json',
-  STATS_QUEUE_URL: process.env.STATS_QUEUE_URL || 'https://queue.status.tfk.allthethings.win?format=json',
   STATS_TIMERSPART_URL: process.env.STATS_TIMERSPART_URL || 'https://timerspart.automate.allthethings.win?format=json',
+  STATUS_QUEUE_URL: process.env.STATUS_QUEUE_URL || 'https://queue.status.tfk.allthethings.win?format=json',
   STATUS_ANTALL_URL: process.env.STATUS_ANTALL_URL || 'https://antall.status.tfk.allthethings.win/?format=json',
   POLITIKER_API: process.env.POLITIKER_API || 'https://politiker-api.t-fk.no/politicians/search/',
   ANSATTE_API: process.env.ANSATTE_API || 'https://search.portalen.t-fk.no/api/employees/search/'
